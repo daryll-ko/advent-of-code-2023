c0dedcd4a155b7af7873349da27a73635a15c15a
Daryll Ko <daryll.ko@gmail.com>
2023-12-06 14:01:32 +0800

    update: remove formula comment

--- 06b.jl
+++ 06b.jl
@@ -10,7 +10,7 @@ function solve()
     for i in eachindex(times)
         # charge for x seconds -> distances of x(t-x)
         # num of x for which x(t-x) > d <-> x^2 - tx + d < 0
-        # (x - t/2)^2 < t^2/4 - d <-> 2 * floor(sqrt(t^2/4 - d) - 1e-5) ways
+        # (x - t/2)^2 < t^2/4 - d
         t, d = times[i], distances[i]
         root = √(t^2/4 - d)
         l, r = ceil(Int, -root + t/2 + 1e-5), floor(Int, root + t/2 - 1e-5)
